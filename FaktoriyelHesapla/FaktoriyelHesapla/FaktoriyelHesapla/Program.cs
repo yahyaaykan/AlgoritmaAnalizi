38c02b8846d2ae6869db292733f100f763d4d06b
YAHYA AYKAN <150533702+yahyaaykan@users.noreply.github.com>
2024-03-21 03:13:03 +0300

    FaktoriyelHesapla Recursive ve İteratif

---
+++ FaktoriyelHesapla/FaktoriyelHesapla/FaktoriyelHesapla/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static int FaktoriyelIteratif(int n)
+    {
+        int sonuc = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            sonuc *= i;
+        }
+        return sonuc;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Faktöriyelini almak istediğiniz sayıyı girin:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        int faktoriyel = FaktoriyelIteratif(sayi);
+        Console.Write("Iteratif Yaklaşım: " + sayi + " sayısının faktöriyeli:  " + faktoriyel);
+        Console.ReadLine();
+    }
+}
---
+++ FaktoriyelHesapla/FaktoriyelHesapla/FaktoriyelHesaplaRecursive/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int FaktoriyelRekursive(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * FaktoriyelRekursive(n - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Faktöriyelini almak istediğiniz sayıyı girin: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        int faktoriyel = FaktoriyelRekursive(sayi);
+        Console.Write("Rekursive Yaklaşım: " + sayi + " sayısının faktöriyeli:  " + faktoriyel);
+        Console.ReadLine();
+    }
+}
