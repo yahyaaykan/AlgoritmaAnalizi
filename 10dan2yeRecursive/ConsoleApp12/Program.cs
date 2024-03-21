8673c09b069a6fc0d78cd811349e3ee9ff597213
YAHYA AYKAN <150533702+yahyaaykan@users.noreply.github.com>
2024-03-21 03:10:10 +0300

    10dan2yeRecursive

---
+++ 10dan2yeRecursive/ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("10'luk sayıyı giriniz: "); // Kullanıcıdan 10'luk bir sayı istenir.
+        int onlukSayi = Convert.ToInt32(Console.ReadLine());
+        string ikilikSayi = OnlukDanIkilikRecursive(onlukSayi);  // Onluk sayıyı ikilik sayı sistemine çevirmek için fonksiyon çağrılır.
+        Console.WriteLine("2'lik sayı sistemi: " + ikilikSayi); // Sonucu ekrana yazdırılır.
+    }
+    static string OnlukDanIkilikRecursive(int onlukSayi) // Onluk sayıyı ikilik sayı sistemine dönüştüren Recursive fonksiyon
+    {
+        if (onlukSayi == 0) // Eğer onluk sayı 0 ise, ikilik sayı da 0'dır.
+        {
+            return "0";
+        }
+        return OnlukDanIkilikRecursive(onlukSayi / 2) + (onlukSayi % 2); // Onluk sayıyı 2'ye böler ve kalanı toplar.
+        
+    }
+}
