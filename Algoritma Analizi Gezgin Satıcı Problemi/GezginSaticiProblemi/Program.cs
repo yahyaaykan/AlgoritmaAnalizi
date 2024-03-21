d0edf15c377e41abe03b1d0c70761726f0f04db4
YAHYA AYKAN <150533702+yahyaaykan@users.noreply.github.com>
2024-03-21 03:05:04 +0300

    GezginSatıcıProblemi

---
+++ Algoritma Analizi Gezgin Satıcı Problemi/GezginSaticiProblemi/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GezginSatıcıProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Şehirlerin koordinatlarını temsil eden bir dizi nokta
+            Point[] sehirler = new Point[]
+            {
+                new Point(0, 0), // Şehir 1
+                new Point(3, 4), // Şehir 2
+                new Point(6, 8), // Şehir 3
+                // ... Diğer şehirler buraya eklenebilir
+            };
+
+            // En kısa turu hesapla
+            List<int> enKisaTur = GezginSatıcıTurunuBul(sehirler);
+
+            // Turu yazdır
+            Console.WriteLine("En kısa tur:");
+            foreach (int sehirIndex in enKisaTur)
+            {
+                Console.Write(sehirIndex + " ");
+            }
+            Console.ReadLine();
+        }
+
+        // İki nokta arasındaki mesafeyi hesaplayan fonksiyon
+        static double Mesafe(Point a, Point b)
+        {
+            double dx = a.X - b.X;
+            double dy = a.Y - b.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        // Gezgin satıcının en kısa turunu bulan fonksiyon
+        static List<int> GezginSatıcıTurunuBul(Point[] sehirler)
+        {
+            int n = sehirler.Length;
+            bool[] ziyaretEdildi = new bool[n];
+            List<int> tur = new List<int>();
+
+            // Başlangıç noktası olarak ilk şehri seç
+            tur.Add(0);
+            ziyaretEdildi[0] = true;
+
+            // Diğer şehirleri ziyaret et
+            for (int i = 1; i < n; i++)
+            {
+                int enYakinSehir = -1;
+                double enKisaMesafe = double.MaxValue;
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (!ziyaretEdildi[j])
+                    {
+                        double mesafe = Mesafe(sehirler[tur[i - 1]], sehirler[j]);
+                        if (mesafe < enKisaMesafe)
+                        {
+                            enKisaMesafe = mesafe;
+                            enYakinSehir = j;
+                        }
+                    }
+                }
+
+                tur.Add(enYakinSehir);
+                ziyaretEdildi[enYakinSehir] = true;
+            }
+
+            // Son şehri başlangıç noktasına dön
+            tur.Add(0);
+
+            return tur;
+        }
+    }
+
+    // Nokta sınıfı
+    class Point
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Algoritma Analizi Gezgin Satıcı Problemi/GezginSaticiProblemiRecursive/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static int N = 4; // Şehir sayısı
+    static int[,] mesafeler = new int[,]
+    {
+        {0, 10, 15, 20},
+        {10, 0, 35, 25},
+        {15, 35, 0, 30},
+        {20, 25, 30, 0}
+    };
+
+    static int EnKisaYolMaliyeti(int mevcutSehir, bool[] ziyaretEdilenSehirler)
+    {
+        // Tüm şehirleri ziyaret ettiğimizi kontrol ediyoruz
+        bool hepsiZiyaretEdildi = true;
+        for (int i = 0; i < N; i++)
+        {
+            if (!ziyaretEdilenSehirler[i])
+            {
+                hepsiZiyaretEdildi = false;
+                break;
+            }
+        }
+        if (hepsiZiyaretEdildi)
+        {
+            return mesafeler[mevcutSehir, 0]; // Başlangıç şehrine dönme maliyeti
+        }
+
+        int minMaliyet = int.MaxValue;
+
+        // Henüz ziyaret edilmemiş şehirleri kontrol ediyoruz
+        for (int sehir = 0; sehir < N; sehir++)
+        {
+            if (!ziyaretEdilenSehirler[sehir])
+            {
+                // Şehri ziyaret ediyoruz
+                ziyaretEdilenSehirler[sehir] = true;
+                // Şehri ziyaret etmek için gereken maliyeti hesaplıyoruz
+                int maliyet = mesafeler[mevcutSehir, sehir] + EnKisaYolMaliyeti(sehir, ziyaretEdilenSehirler);
+                // Minimum maliyeti güncelliyoruz
+                minMaliyet = Math.Min(minMaliyet, maliyet);
+                // Geri dönüşte şehri ziyaret etmemizi kaldırıyoruz
+                ziyaretEdilenSehirler[sehir] = false;
+            }
+        }
+
+        return minMaliyet;
+    }
+
+    static void Main(string[] args)
+    {
+        bool[] ziyaretEdilenSehirler = new bool[N];
+        ziyaretEdilenSehirler[0] = true; // Baş
+    }
+}
