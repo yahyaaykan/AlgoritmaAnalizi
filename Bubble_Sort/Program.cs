a0629bcfe28941a072621df3354ddf5bc093198e
YAHYA AYKAN <150533702+yahyaaykan@users.noreply.github.com>
2024-03-21 03:20:09 +0300

    Bubble_Sort

---
+++ Bubble_Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int[] deneme = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("{0}. Sayiyi Giriniz: ", i + 1);
+    deneme[i] = int.Parse(Console.ReadLine());
+
+}
+
+int n = deneme.Length;
+int kntrl = 0, tut;
+
+for (int i = 0; i < n - 1; i++)
+{
+    for (int j = 0; j < n - kntrl - 1; j++)
+    {
+        if (deneme[j] > deneme[j + 1])
+        {
+            tut = deneme[j];
+            deneme[j] = deneme[j + 1];
+            deneme[j + 1] = tut;
+        }
+    }
+}
+
+
+foreach (int i in deneme)
+{
+    Console.Write(i + "  ");
+}
